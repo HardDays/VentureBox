28f74173576aa27c3d83328f6d27a7dc039da2d9
snolly <snolly.gv@gmail.com>
2019-07-09 11:22:50 +0300

    -feat: add tracking page

--- src/app/system/tracking/tracking.component.ts
+++ src/app/system/tracking/tracking.component.ts
@@ -2,6 +2,18 @@ import { AuthService } from './../../core/services/auth.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { TrackingService } from 'src/app/core/services/tracking.service';
 
+interface InvestorsModel {
+    name: string;
+    months: [{
+      title: string,
+      amount: number,
+      payed: boolean
+    }];
+    total_investment: number;
+    debt: number;
+    company_id: number;
+}
+
 @Component({
   selector: 'app-tracking',
   templateUrl: './tracking.component.html',
@@ -11,7 +23,8 @@ export class TrackingComponent implements OnInit {
 
   MyRole = '';
   Axis: string[] = [];
-  Investors: any[] = [];
+  Investors: InvestorsModel[] = [];
+  Companies: InvestorsModel[] = [];
 
   isOpenModal = false;
   Date = '';
@@ -38,8 +51,10 @@ export class TrackingComponent implements OnInit {
         this.MyRole === 'startup' ? this.MyRole : 'investor',
         (res) => {
           this.Axis = res.axis;
-          this.Investors = res.investors;
-          console.log(`Investors = `, this.Investors);
+          if(this.MyRole === 'startup')
+            this.GetInvestorsInfo(res.investors);
+          else 
+            this.GetInvestorsInfo(res.companies);
         },
         (err) => {
           console.log(`err = `, err);
@@ -48,11 +63,49 @@ export class TrackingComponent implements OnInit {
     }
   }
 
+  GetInvestorsInfo (investors) {
+    this.Investors = [];
+    // tslint:disable-next-line: forin
+    for (const item in investors) {
+      const tmp: InvestorsModel = {
+        name: '',
+        months: [{
+          title: '',
+          amount: 0,
+          payed: false
+        }],
+        total_investment: 0,
+        debt: 0,
+        company_id: 0
+      };
+      tmp.name = item;
+      tmp.debt = investors[item].debt;
+      tmp.total_investment = investors[item].total_investment;
+      if(investors[item].company_id)
+        tmp.company_id = investors[item].company_id;
+
+      for (const month of this.Axis) {
+        for (const info in investors[item]) {
+          if (info.length && month === info) {
+             tmp.months.push({
+               title: info,
+               payed: investors[item][info]['payed'],
+               amount: investors[item][info]['amount']
+             });
+          }
+        }
+      }
+
+      this.Investors.push(tmp);
+    }
+  }
+
+  
+
   OpenModal (dateId: number, companyId: number) {
     this.isOpenModal = true;
     this.Date = this.Axis[dateId];
     this.CompanyId = companyId;
-    console.log(this.Date, this.CompanyId);
   }
 
   PaidToProject() {
@@ -61,7 +114,7 @@ export class TrackingComponent implements OnInit {
       this.Date,
       this.CompanyId,
       (res) => {
-        console.log(`Success: `, res);
+        this.GetTrackingList();
       }
     );
   }
